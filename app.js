81d5afe099be68df1f79630b4ab880fd44c9a6e8
Treast <vincent.riva@outlook.fr>
2019-02-11 23:36:04 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,110 @@
+const tf = require('@tensorflow/tfjs');
+
+tf.disableDeprecationWarnings();
+
+require('@tensorflow/tfjs-node');
+const NodeWebcam = require('node-webcam');
+const Posenet = require('@tensorflow-models/posenet');
+global.XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const requestAnimationFrame = require('raf');
+const { createCanvas, Image } = require('canvas');
+
+const posenetConfiguration = {
+  imageScaleFactor: 0.6,
+  multiplier: 0.75,
+  outputStride: 16,
+  reversed: false,
+};
+
+const canvas = createCanvas(1920, 1080);
+const ctx = canvas.getContext('2d');
+
+let webcam = null;
+let net = null;
+
+const initWebcam = () => {
+  return new Promise((resolve) => {
+    webcam = NodeWebcam.create({
+      callbackReturn: 'base64',
+    });
+    console.log('Webcam resolved');
+    resolve();
+  });
+};
+
+const initModel = () => {
+  return new Promise((resolve, reject) => {
+    Posenet.load(posenetConfiguration.multiplier)
+      .then((posenet) => {
+        net = posenet;
+        console.log('Posenet resolved');
+        resolve();
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+const getHand = (data) => {
+  return new Promise((resolve, reject) => {
+    net
+      .estimateSinglePose(canvas, posenetConfiguration.imageScaleFactor, posenetConfiguration.reversed, posenetConfiguration.outputStride)
+      .then((pose) => {
+        const handKeyPoints = pose.keypoints.filter((item) => {
+          return item.part === 'rightWrist' || item.part === 'leftWrist';
+        });
+
+        handKeyPoints.sort((a, b) => {
+          return a.score > b.score ? 1 : -1;
+        });
+
+        resolve(this.getPartLocation(handKeyPoints[0]));
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+const loadImage = (base64) => {
+  return new Promise((resolve) => {
+    const image = new Image();
+    image.onload = () => {
+      ctx.drawImage(image, 0, 0, 1920, 1080);
+      resolve(image);
+    };
+    image.onerror = (err) => {
+      throw err;
+    };
+
+    image.src = base64;
+  });
+};
+
+const render = () => {
+  webcam.capture('webcam', (err, data) => {
+    if (err) {
+      console.error(err);
+      return false;
+    }
+
+    loadImage(data).then((image) => {
+      // const tfImage = tf.browser.fromPixels(canvas);
+
+      getHand(canvas).then((hand) => {
+        console.log(hand.position);
+        requestAnimationFrame(render());
+      });
+    });
+  });
+};
+
+initWebcam()
+  .then(() => initModel())
+  .then(() => {
+    render();
+  })
+  .catch((err) => {
+    console.error(err);
+  });
